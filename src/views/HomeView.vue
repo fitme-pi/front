9357ccc83bec1283803da8ca6d50fa2edcdffb28
Fitme <fitmepi@gmail.com>
2022-11-06 22:56:05 -0300

    testando acesso restrito para staff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-container class="pa-8" fluid>
+  <v-container v-if="user ? user.is_staff : false" class="pa-8" fluid>
     <h1 class="h1">Fitme</h1>
     <v-form>
       <v-text-field
-        color="white"
+        dark
         label="Nova tarefa"
         v-model="novaTarefa"
         @keydown.enter="adicionar"
@@ -12,9 +12,9 @@
       </v-text-field>
     </v-form>
     <v-divider></v-divider>
-    <v-list>
+    <v-list v-for="tarefa of tarefas" :key="tarefa.titulo">
       <v-list-item-group>
-        <v-list-item v-for="tarefa of tarefas" :key="tarefa.titulo">
+        <v-list-item>
           {{ tarefa.titulo }}
         </v-list-item>
       </v-list-item-group>
@@ -23,12 +23,17 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   data() {
     return {
       novaTarefa: "",
       tarefas: [],
     };
+  },
+  computed: {
+    ...mapState("auth", ["user"]),
   },
   methods: {
     adicionar(e) {
