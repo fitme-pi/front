275fa2f4fe595722946ebc8aed958f1f509400ea
Fitme <fitmepi@gmail.com>
2022-12-15 09:49:19 -0300

    edição de exercício

---
+++ src/components/EditarExercicio.vue
@@ -0,0 +1,133 @@
+<template>
+  <v-row justify="center">
+    <v-dialog class="a" v-model="$parent.editar" width="600px">
+      <v-form class="d-flex align-center flex-column">
+        <h1 class="h1 py-8 pl-5">Edite o exercício</h1>
+        <v-col cols="12">
+          <v-text-field
+            outlined
+            v-model="exercicio.nome"
+            cols="12"
+            sm="6"
+            md="3"
+            dark
+            label="Nome do exercício"
+          >
+          </v-text-field>
+        </v-col>
+        <v-col cols="12">
+          <v-textarea
+            outlined
+            v-model="exercicio.descricao"
+            :rules="rules"
+            cols="12"
+            sm="6"
+            md="3"
+            dark
+            label="Descrição do exercício"
+          ></v-textarea>
+        </v-col>
+        <v-col cols="12">
+          <v-text-field
+            outlined
+            v-model="exercicio.video_explicativo"
+            cols="12"
+            sm="6"
+            md="3"
+            dark
+            label="Video Explicativo do exercício"
+          ></v-text-field>
+        </v-col>
+        <v-col class="d-flex" cols="12">
+          <v-select
+            dark
+            cols="12"
+            sm="6"
+            md="3"
+            v-model="grupo"
+            :items="grupos"
+            item-text="nome"
+            item-value="id"
+            label="Grupo Muscular do exercício"
+            outlined
+          ></v-select>
+        </v-col>
+        <v-row class="mb-5">
+          <div class="ma-3 mt-3">
+            <v-btn @click="salvarAlteraçoes()">
+              <h3>Publicar</h3>
+            </v-btn>
+          </div>
+          <v-spacer></v-spacer>
+          <div class="ma-3 mt-3">
+            <v-btn @click="$parent.editar = false">
+              <h3>Cancelar</h3>
+            </v-btn>
+          </div>
+        </v-row>
+      </v-form>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: ["exercicio_prop"],
+  data() {
+    return {
+      exercicio: this.exercicio_prop,
+      grupo: {},
+      grupos: [
+        {
+          id: 1,
+          nome: "Costas",
+        },
+        {
+          id: 2,
+          nome: "Bíceps",
+        },
+        {
+          id: 3,
+          nome: "Ombros",
+        },
+        {
+          id: 4,
+          nome: "Glúteos",
+        },
+        {
+          id: 5,
+          nome: "Pernas",
+        },
+        {
+          id: 6,
+          nome: "Peito",
+        },
+        {
+          id: 7,
+          nome: "Tríceps",
+        },
+        {
+          id: 8,
+          nome: "Abdômen",
+        },
+      ],
+      rules: [
+        (v) => (v ? v.length <= 500 : false || "No máximo 500 caracteres"),
+      ],
+    };
+  },
+  watch: {
+    exercicio_prop() {
+      this.exercicio = this.exercicio_prop;
+      this.grupo = this.exercicio_prop.grupo_muscular;
+    },
+  },
+  methods: {
+    salvarAlteraçoes() {
+      this.exercicio.grupo_muscular = this.grupo;
+      this.$emit("salvar", this.exercicio);
+      this.$parent.editar = false;
+    },
+  },
+};
+</script>
--- src/components/ExercicioCard.vue
+++ src/components/ExercicioCard.vue
@@ -37,6 +37,10 @@
           <v-divider class="mx-4"></v-divider>
 
           <v-card-actions v-if="user && user.is_staff">
+            <v-btn color="blue" text @click="popupEdicao(exercicio)">
+              Editar
+            </v-btn>
+            <v-spacer></v-spacer>
             <v-btn color="warning" text @click="deletarExercicio(exercicio)">
               Excluir
             </v-btn>
@@ -44,19 +48,29 @@
         </v-card>
       </v-col>
     </v-row>
+    <EditarExercicio
+      @salvar="editarExercicio"
+      :exercicio_prop.sync="exercicio"
+    />
   </v-container>
 </template>
 
 <script>
 import axios from "axios";
 import { mapState } from "vuex";
+import EditarExercicio from "@/components/EditarExercicio.vue";
 
 export default {
+  components: {
+    EditarExercicio,
+  },
   created() {
     this.pegarExercicios();
   },
   data: () => ({
     exercicios: [],
+    exercicio: {},
+    editar: false,
     loading: false,
   }),
   computed: {
@@ -67,19 +81,30 @@ export default {
       const { data } = await axios.get("api/exercicios/");
       this.exercicios = data;
     },
-
-    tratarURL(url) {
-      return `${url.replace(".com/", ".com/embed/").replace("watch?v=", "")}`;
+    async editarExercicio(exercicio) {
+      try {
+        await axios.patch(`api/exercicios/${exercicio.id}/`, exercicio);
+        this.pegarExercicios();
+      } catch (e) {
+        console.log(e);
+      }
     },
-
     async deletarExercicio({ id }) {
       try {
         await axios.delete(`api/exercicios/${id}/`);
+        this.pegarExercicios();
       } catch (e) {
         console.log(e);
       }
     },
 
+    popupEdicao(exercicio) {
+      this.exercicio = exercicio;
+      this.editar = true;
+    },
+    tratarURL(url) {
+      return `${url.replace(".com/", ".com/embed/").replace("watch?v=", "")}`;
+    },
     reserve() {
       this.loading = true;
 
